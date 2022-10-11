97419b206fcfaf86ab0fda72bbe7dc38ca1df304
Adam Cooper <adam@adamcooper.sh>
2022-10-11 11:17:51 -0500

    Default to most recent puzzle when `play` is called with no arguments

--- src/main/kotlin/sh/adamcooper/wordle/Main.kt
+++ src/main/kotlin/sh/adamcooper/wordle/Main.kt
@@ -29,10 +29,10 @@ fun main(args: Array<String>) {
         }
 
         "play" -> {
-            if (args.size != 2) {
+            if (args.size > 2) {
                 errorExit()
             }
-            val answer = args[1].uppercase()
+            val answer = (args.getOrNull(1) ?: wordleAnswer(wordleCount())).uppercase()
             val board = playWordle(
                 words,
                 findBestWords(words, n = 1, uniqueOnly = true).asSequence().first().key,
